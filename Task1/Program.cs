c84c5814c3d76db7c25d07179cf61e59a560a86c
Fedor <fedor_prg@rambler.ru>
2022-05-24 22:14:15 +1000

    Updated Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,62 +1,37 @@
-﻿void PrintArray(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            if (matr[i, j] != 0) Console.Write($"{matr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
+﻿Console.Clear();
 Console.WriteLine("Введите число N: ");
 int n = int.Parse(Console.ReadLine()!);
 bool exit = false;
-int[] row = new int[n + 2];
-row[0] = 1;
-for (int i = 1; i < n; i++)
-{
-    row[i] = row[i - 1] + 1;
-    /* Console.Write(row[i] + " "); */
-}
-Console.WriteLine();
-int[,] group = new int[n + 1, n + 1];
-int k = 1;
-group[0, 0] = 1;
-
+int k = 2;
+int[] group = new int[n];
+group[0] = k - 1;
+Console.Write($"-Группа {1}: " + group[0]);
 for (int i = 1; i <= n; i++)
 {
-    if (row[k] == 0) break;
+    if (k >= n) break;
     else
     {
-        group[i, 0] = row[k];
-        /* Console.WriteLine($"group[{i}, 0]={row[k]}"); */
+        Console.WriteLine();
+        group[0] = k;
+        Console.Write($"-Группа {i + 1}: " + group[0]);
         for (int j = 1; j < n; j++)
         {
-            /* Console.WriteLine("row[k]=" + row[k]); Console.ReadLine(); */
             for (int m = 0; m < j; m++)
             {
-                /* Console.WriteLine("row[k+1] % group[i, m]=" + row[k + 1] + "%" + group[i, m] + "=" + row[k + 1] % group[i, m]);
-                Console.ReadLine(); */
-                if (row[k + 1] % group[i, m] == 0)
+                if ((k + 1) > n | (k + 1) % group[m] == 0)
                 {
                     exit = true;
-                    j = n; /* Console.WriteLine(j); */
+                    j = n;
                     k = k + 1; break;
                 }
                 else exit = false;
             }
             if (exit == false)
             {
-                group[i, j] = row[k + 1];
+                group[j] = k + 1;
                 k = k + 1;
-                /* Console.WriteLine($"group[{i},{j}]=" + group[i, j] + " " + k); */
+                Console.Write(" " + group[j]);
             }
         }
     }
 }
-
-
-PrintArray(group);
-
